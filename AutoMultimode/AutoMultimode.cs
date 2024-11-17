872e5a3e2da69d716419f9e828f12ff2a2d48d45
Alexis Tan <alexis@sen-dev.com>
2024-11-17 16:02:57 +0100

    Rename plugin to AutoMultimode

---
+++ AutoMultimode/AutoMultimode.cs
@@ -0,0 +1,55 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using SamplePlugin.Windows;
+
+namespace SamplePlugin;
+
+public sealed class AutoMultimode : IDalamudPlugin
+{
+    [PluginService]
+    internal static IDalamudPluginInterface PluginInterface { get; private set; } = null!;
+
+    [PluginService]
+    internal static ICommandManager CommandManager { get; private set; } = null!;
+
+    private const string CommandName = "/automultimode";
+    private const string PluginName = "Auto Multimode";
+
+    public readonly WindowSystem WindowSystem = new(PluginName);
+    private InformationWindow InformationWindow { get; init; }
+
+    public AutoMultimode()
+    {
+        InformationWindow = new InformationWindow();
+        WindowSystem.AddWindow(InformationWindow);
+
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "Displays some information about the plugin.",
+            ShowInHelp = true,
+        });
+
+        PluginInterface.UiBuilder.Draw += DrawUI;
+        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
+    }
+
+    public void Dispose()
+    {
+        WindowSystem.RemoveAllWindows();
+        InformationWindow.Dispose();
+
+        CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        ToggleMainUI();
+    }
+
+    private void DrawUI() => WindowSystem.Draw();
+    public void ToggleMainUI() => InformationWindow.Toggle();
+}
