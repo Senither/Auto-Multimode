075919ac96ddc8a1308c8c2b903d885e1d2f9ff3
Alexis Tan <alexis@sen-dev.com>
2024-11-17 16:23:33 +0100

    Remove unused classes and files from the plugin template

---
+++ AutoMultimode/Windows/InformationWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Internal;
+using Dalamud.Interface.Utility;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using ImGuiNET;
+
+namespace SamplePlugin.Windows;
+
+public class InformationWindow : Window, IDisposable
+{
+    public InformationWindow() : base(
+        name: "Auto Multimode##information-window",
+        flags: ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse
+    )
+    {
+        SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(375, 330),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        ImGui.Text($"Hello from the information window.");
+    }
+}
